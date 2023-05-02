68b69304779c9fb11e8a078d376e339b06bfda8f
Leandro da Silva Soares <3973@ucdb.br>
2023-05-02 08:32:04 -0400

    Utilizando Data Annotatios para validar dados recebidos

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -1,10 +1,16 @@
-﻿namespace FilmesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
 {
     public class Filme
     {
+        [Required(ErrorMessage = "O campo Título é obrigatório.")]
         public string Titulo { get; set; }
+        [StringLength(30, ErrorMessage = "O Gênero deve conter no máximo 30 caracteres.")]
         public string Genero { get; set; }
+        [Required(ErrorMessage = "O campo Diretor é obrigatório.")]
         public string Diretor { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve ter entre 1 e 600 minutos.")]
         public int Duracao { get; set; }
     }
 }
