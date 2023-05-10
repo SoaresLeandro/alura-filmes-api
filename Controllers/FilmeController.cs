c45bd20de99bb999b11e2d154d762c52ad633d50
Leandro da Silva Soares <3973@ucdb.br>
2023-05-10 14:40:56 -0400

    Atualizando e Removendo Dados

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -37,5 +37,36 @@ namespace FilmesAPI.Controllers
 
             return filme is not null ? Ok(filme) : NotFound();
         }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaFilme([FromBody] Filme filmeParaAtualizar, int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if(filme is null) return NotFound();
+
+            filme.Titulo = filmeParaAtualizar.Titulo;
+            filme.Diretor = filmeParaAtualizar.Diretor;
+            filme.Genero = filmeParaAtualizar.Genero;
+            filme.Duracao = filmeParaAtualizar.Duracao;
+
+            _context.Update(filme);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaFilme(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if(filme is null) return NotFound();
+
+            _context.Filmes.Remove(filme);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
