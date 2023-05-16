910065234eaabd496f165c6624bd1969231e95bd
Leandro da Silva Soares <3973@ucdb.br>
2023-05-16 13:38:14 -0400

    Adicionando Endereco

---
+++ Controllers/EnderecoController.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class EnderecoController : ControllerBase
+    {
+        private FilmeContext _context;
+        private IMapper _mapper;
+
+        public EnderecoController(FilmeContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult AdiconarEndereco([FromBody] CreateEnderecoDto enderecoDto)
+        {
+            Endereco endereco = _mapper.Map<Endereco>(enderecoDto);
+            _context.Enderecos.Add(endereco);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(RecuperarEnderecoPorId), new { Id = endereco.Id }, endereco);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperarEnderecoPorId(int id)
+        {
+            Endereco endereco = _context.Enderecos.FirstOrDefault(endereco => endereco.Id == id);
+
+            return endereco is null ? NotFound() : Ok(_mapper.Map<ReadEnderecoDto>(endereco));
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarEnderecos()
+        {
+            return Ok(_mapper.Map<List<ReadEnderecoDto>>(_context.Enderecos.ToList()));
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizarEndereco([FromBody] UpdateEnderecoDto enderecoDto, int id)
+        {
+            Endereco endereco = _context.Enderecos.FirstOrDefault(endereco => endereco.Id == id);
+
+            if(endereco is null) return NotFound();
+
+            _mapper.Map(endereco, enderecoDto);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult ExcluirEndereco(int id)
+        {
+            Endereco endereco = _context.Enderecos.FirstOrDefault(endereco => endereco.Id == id);
+
+            if(endereco is null) return NotFound();
+
+            _context.Remove(endereco);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Profiles/EnderecoProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Profiles
+{
+    public class EnderecoProfile : Profile
+    {
+        public EnderecoProfile()
+        {
+            CreateMap<Endereco, CreateEnderecoDto>().ReverseMap();
+            CreateMap<Endereco, ReadEnderecoDto>().ReverseMap();
+            CreateMap<Endereco, UpdateEnderecoDto>().ReverseMap();
+        }
+    }
+}
