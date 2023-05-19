c5607ac736d7247fee41d2ec2fa3c28b103efb47
Leandro da Silva Soares <3973@ucdb.br>
2023-05-19 13:38:05 -0400

    Criando Sessão

---
+++ Controllers/SessaoController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("{Controller}")]
+    public class SessaoController : ControllerBase
+    {
+        private FilmeContext _context;
+        private IMapper _mapper;
+
+        public SessaoController(FilmeContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarSessao([FromBody] CreateSessaoDto sessaoDto)
+        {
+            Sessao sessao = _mapper.Map<Sessao>(sessaoDto);
+            _context.Sessoes.Add(sessao);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(RecuperarSessaoPorId), new { id = sessao.Id }, sessao);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarSessoes()
+        {
+            return Ok(_mapper.Map<List<ReadSessaoDto>>(_context.Sessoes.ToList()));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperarSessaoPorId(int id) 
+        {
+            Sessao sessao = _context.Sessoes.FirstOrDefault(sessao => sessao.Id == id);
+
+            return sessao is null ? NotFound() : Ok(_mapper.Map<ReadSessaoDto>(sessao));
+        }
+    }
+}
---
+++ Profiles/SessaoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Profiles
+{
+    public class SessaoProfile : Profile
+    {
+        public SessaoProfile()
+        {
+            CreateMap<Sessao, CreateSessaoDto>().ReverseMap();
+            CreateMap<Sessao, ReadSessaoDto>().ReverseMap();
+        }
+    }
+}
